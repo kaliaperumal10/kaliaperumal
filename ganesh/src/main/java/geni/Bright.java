d2ed8d231a6ed39b3dddb05b84235c21f02fa897
kaliaperumal10 <104897168+kaliaperumal10@users.noreply.github.com>
2022-05-05 13:15:22 +0530

    dd

---
+++ ganesh/src/main/java/geni/Bright.java
@@ -0,0 +1,19 @@
+package geni;
+import java.io.*;
+
+		import org.apache.log4j.Logger;
+		import org.apache.log4j.PropertyConfigurator;
+		public class Bright {
+			static Logger logger=Logger.getLogger(Bright.class);
+			
+			public static void main(String[] args) {
+				String str="welcome to sonar";
+				
+				String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+				PropertyConfigurator.configure(log4jConfigFile);
+				logger.info("this is a message....:"+str);
+				logger.warn("warning message...:"+str);
+				logger.error("error message...:"+str);
+			}
+		}
+	
