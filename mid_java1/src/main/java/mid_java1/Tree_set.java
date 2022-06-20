13d05a4f398ba63352366775036c2c0183cdfac6
kaliaperumal10 <104897168+kaliaperumal10@users.noreply.github.com>
2022-06-20 17:59:38 +0530

    mapHT & Weak

--- mid_java1/src/main/java/mid_java1/Tree_set.java
+++ mid_java1/src/main/java/mid_java1/Tree_set.java
@@ -14,11 +14,13 @@ public class Tree_set {
 //		ts.add("2");
 //		System.out.println(ts);
 		TreeSet<String> ts1=new TreeSet<String>(new Mycmp());
+		// TreeSet<String> res = (TreeSet<String>)ts1.descendingSet();
 		ts1.add("4");
 		ts1.add("1");
 		ts1.add("3");
 		ts1.add("2");
 		
+	//	System.out.println(res);
 		System.out.println(ts1);
 	}
 }
@@ -27,12 +29,8 @@ class Mycmp implements Comparator<String>{
    
     public int compare(String c1, String c2)
     {
-    	
-    	
-    		String fts;
-    		String fts1;
-    		fts=c1;
-    		fts1=c2;
-        return fts1.compareTo(fts);
+ System.out.println(c1);
+ System.out.println(c2);
+        return c2.compareTo(c1);
     }
 }
