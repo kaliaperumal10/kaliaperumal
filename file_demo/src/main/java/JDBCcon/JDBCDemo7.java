8c7be7c9119a8b2c5e08fd00f389a2399c75250b
kaliaperumal10 <104897168+kaliaperumal10@users.noreply.github.com>
2022-06-27 12:56:05 +0530

    procedure

---
+++ file_demo/src/main/java/JDBCcon/JDBCDemo7.java
@@ -0,0 +1,25 @@
+package JDBCcon;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+//import java.sql.Statement;
+public class JDBCDemo7 {
+	public static void main(String[] args) throws Exception{
+		//Step1 - Load the Driver.
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//Step 2 - Establish Connection
+		Connection con=DriverManager.getConnection
+				("jdbc:mysql://localhost:3306/anoud","root","root");
+		
+		System.out.println(con);
+		
+		DatabaseMetaData dbmd=con.getMetaData();
+		
+		System.out.println(dbmd.getDatabaseProductName());
+		System.out.println(dbmd.getDatabaseProductVersion());
+		
+		System.out.println(dbmd.getUserName());
+		
+	}
+}
